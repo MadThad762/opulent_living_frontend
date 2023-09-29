b73cca9b381ad85ea0d7bf4a2afb34148115510c
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-09-28 20:16:11 -0400

    started on the property details page

--- app/routes/properties.$id.tsx
+++ app/routes/properties.$id.tsx
@@ -1,7 +1,12 @@
+import { Link, useLoaderData } from '@remix-run/react';
+import type { Property } from '~/types/PropertyTypes';
 import { json } from '@remix-run/node';
 import type { V2_MetaFunction, LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import type { Property } from '~/types/PropertyTypes';
+
+interface PropertyCardProps {
+  property: Property;
+  baseURL: string;
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -29,18 +34,51 @@ export const loader: LoaderFunction = async ({ params }) => {
   }
 };
 
-export default function PropertyDetails() {
+export default function PropertyDetails({ baseURL }: PropertyCardProps) {
   const property: Property = useLoaderData();
 
   return (
-    <div className='mx-auto w-full'>
-      <img
-        src={`https://res.cloudinary.com/dwfmymy4z/image/upload/w_2000,q_75/v1695873487/property_listings/${property?.imagePublicId}.jpg`}
-        alt={property?.description}
-      />
-      <p>{property?.title}</p>
-      <p>{property?.description}</p>
-      <p>{property?.price}</p>
+    <div className='flex flex-col w-full h-auto max-w-7xl mx-auto mt-16'>
+      <div className='relative group aspect-w-16 aspect-h-10 w-full h-full'>
+        <img
+          src={`https://res.cloudinary.com/dwfmymy4z/image/upload/w_2000,q_75/v1695873487/property_listings/${property?.imagePublicId}.jpg`}
+          alt={property?.description}
+          className='h-full w-full object-cover'
+        />
+        {property?.isFeatured && (
+          <div className='absolute h-full w-full'>
+            <div className='bg-white/50 mt-3 ml-3 flex w-min'>
+              <span className='text-black text-xs tracking-widest bg-transparent rounded-full px-2.5 py-0.5'>
+                FEATURED
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className='flex flex-col space-y-1'>
+        <Link to={`/properties/${property?.id}`}>
+          <h3 className='text-base uppercase tracking-widest mt-4 whitespace-nowrap truncate'>
+            {property?.title}
+          </h3>
+        </Link>
+        <ol className='flex flex-row space-x-1.5 text-sm'>
+          <li>{property?.numberOfBeds} bds</li>
+          <span>|</span>
+          <li>{property?.numberOfBaths} ba</li>
+          <span>|</span>
+          <li>{property?.sqft} sqft</li>
+          <span> | </span>
+          <li>{property?.propertyType} for sale</li>
+        </ol>
+        <p className='text-base font-medium'>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+          }).format(Number(property?.price))}
+        </p>
+      </div>
     </div>
   );
 }
